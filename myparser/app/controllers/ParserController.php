994247e6e5bb7a73bf4a082a3f81170432a9a20d
kubitckiy_bogdan <bk@albedo.dev>
2021-02-08 14:30:55 +0200

    added ProxyContoller and ThreadController

--- myparser/app/controllers/ParserController.php
+++ myparser/app/controllers/ParserController.php
@@ -9,4 +9,18 @@ use app\core\Controller;
 class ParserController extends Controller
 {
 
+    private $url;
+    protected $config;
+    private $count = 0;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->url = $this->config['url'];
+
+        if(empty($this->url)){
+            throw new \Exception("Адрес сайта пустой.");
+        }
+    }
+
 }
--- myparser/app/core/Controller.php
+++ myparser/app/core/Controller.php
@@ -7,6 +7,15 @@ use app\interfaces\IParser;
 class Controller implements IParser
 {
 
+    protected $config;
+
+    public function __construct()
+    {
+        $this->config = require "../config/config.php";
+
+
+    }
+
     public function getPage(string $url) {
 
     }
